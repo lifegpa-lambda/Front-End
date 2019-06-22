4ac8c93a928690198ca25a06f2b7080260a3bb80
Rory Murray <rory.christian.murray@gmail.com>
2019-06-22 14:59:21 -0400

    make authActions.js

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -3,3 +3,18 @@ import axios from "axios";
 export const LOGIN_START = "LOGIN_START";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_ERROR = "LOGIN_ERROR";
+
+export const login = creds => dispatch => {
+  dispatch({ type: LOGIN_START });
+  return axios
+    .post("http://localhost:5000/api/login", creds)
+    .then(response => {
+      console.log("login response", response);
+      localStorage.setItem("token", response.data.payload);
+      dispatch({ type: LOGIN_SUCCESS, payload: response.data.payload });
+    })
+    .catch(error => {
+      console.log("login error.response", error.response);
+      dispatch({ type: LOGIN_ERROR, payload: error.response.data.error });
+    });
+};
