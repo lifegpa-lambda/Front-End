3d46ecaeaad05fdba17f28dbc66dd037cb878cd8
Rory Murray <rory.christian.murray@gmail.com>
2019-06-22 15:35:07 -0400

    add Router, render Login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
+import Login from "./components/Login";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Welcome to LifeGPA!</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <ul className="links">
+          <li>
+            <Link to="/login">Log In</Link>
+          </li>
+          <li>
+            <Link to="/habits">Habits</Link>
+          </li>
+        </ul>
+        <Route path="/login" component={Login} />
+        {/* <PrivateRoute exact path="/habits" component={HabitList} /> */}
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { login } from "../actions/authActions"
+import { login } from "../actions/authActions";
 
 class Login extends Component {
   state = {
@@ -11,61 +11,60 @@ class Login extends Component {
   };
 
   handleChanges = event => {
-      event.preventDefault();
-      this.setState({
-          credentials: {
-              this.state.credentials,
-              [event.target.name]: event.target.value
-          }
-      });
-  }
+    event.preventDefault();
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [event.target.name]: event.target.value
+      }
+    });
+  };
 
   login = event => {
     console.log("login event", event);
     event.preventDefault();
-    this.props.login(this.state.credentials)
-    .then(() => {
-        this.props.history.push('/habit-list');
+    this.props.login(this.state.credentials).then(() => {
+      this.props.history.push("/habit-list");
     });
 
     this.setState({
-        username: "",
-        password: ""
+      username: "",
+      password: ""
     });
   };
 
   render() {
     return (
+      <div>
+        <h1>Welcome to LifeGPA!</h1>
         <form onSubmit={this.login}>
-            <input 
+          <input
             type="text"
             name="username"
             placeholder="username"
             value={this.state.credentials.username}
             onChange={this.handleChanges}
-            />
-            <input 
+          />
+          <input
             type="password"
             name="password"
             placeholder="password"
             value={this.state.credentials.password}
             onChange={this.handleChanges}
-            />
-            <button>Create Account</button>
-            <button>Log In</button>
+          />
+          <button>Log In</button>
         </form>
-
+      </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-    error: state.user.error,
-    fetching: state.user.fetching
-  });
+  error: state.user.error,
+  fetching: state.user.fetching
+});
 
-
-  export default connect(
-    mapStateToProps,
-    { login }
-  )(Login);
+export default connect(
+  mapStateToProps,
+  { login }
+)(Login);
