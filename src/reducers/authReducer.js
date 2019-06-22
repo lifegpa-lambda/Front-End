3b42e367e1528f823a746bd427857872bfe58702
Rory Murray <rory.christian.murray@gmail.com>
2019-06-22 15:05:23 -0400

    Combine reducers

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -4,42 +4,39 @@ import {
   LOGIN_ERROR
 } from "../actions/authActions";
 
-
 const initialState = {
-    habits: [],
-    fetching: false,
-    error: null,
-    loading: true,
-    loggingIn: false
-    token: localStorage.getItem('token')
-
-
-}
+  habits: [],
+  fetching: false,
+  error: null,
+  loading: true,
+  loggingIn: false,
+  token: localStorage.getItem("token")
+};
 
 export default (state = initialState, action) => {
-    switch (action.type) {
-        case LOGIN_START: 
-        console.log("Auth reducer state", state);
-        console.log("Auth reducer action", action);
-        return {
-            ...state
-            loggingIn: true,
-            error: null,
-        }
-        case LOGIN_SUCCESS:
-            return {
-                ...state,
-                loggingIn: false,
-                error: null,
-                token: localStorage.getItem('token')
-
-            }
-            case LOGIN_ERROR:
-                return {
-                    ...state,
-                    loggingIn: false,
-                    error: action.payload
-                }
-                default: return state;
-    };
-} 
+  switch (action.type) {
+    case LOGIN_START:
+      console.log("Auth reducer state", state);
+      console.log("Auth reducer action", action);
+      return {
+        ...state,
+        loggingIn: true,
+        error: null
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        loggingIn: false,
+        error: null,
+        token: localStorage.getItem("token")
+      };
+    case LOGIN_ERROR:
+      return {
+        ...state,
+        loggingIn: false,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+};
